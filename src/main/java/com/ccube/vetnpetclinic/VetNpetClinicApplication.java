712554eaa4e17da247c3d2d23ff9c3d44f340eb2
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-13 22:40:02 +0530

    initial commit

---
+++ src/main/java/com/ccube/vetnpetclinic/VetNpetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.ccube.vetnpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetNpetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetNpetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccube/vetnpetclinic/VetNpetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccube.vetnpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetNpetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
