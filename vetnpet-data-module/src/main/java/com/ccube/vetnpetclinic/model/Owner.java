6de17e325d0e1f179aaedee78373683b9b3341cb
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-15 00:43:24 +0530

    added base entity class for unique id's. Closes #10.

--- vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/model/Owner.java
+++ vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/model/Owner.java
@@ -1,4 +1,34 @@
 package com.ccube.vetnpetclinic.model;
 
 public class Owner extends Person {
+
+    private String address;
+
+    private String city;
+
+    private String telephone;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 }
--- vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/model/PetType.java
+++ vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.ccube.vetnpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
