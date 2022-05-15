aafb563436129c72abe838d7de6887ec67982749
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-15 16:46:57 +0530

    added single common base interface for all interfaces closes #10

--- vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/service/OwnerService.java
+++ vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/service/OwnerService.java
@@ -2,15 +2,7 @@ package com.ccube.vetnpetclinic.service;
 
 import com.ccube.vetnpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
-
-    Owner Save(Owner owner);
-
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findOwnerByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Set<Owner> findAll(Owner owner);
 }
--- vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/service/PetService.java
+++ vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/service/PetService.java
@@ -2,13 +2,7 @@ package com.ccube.vetnpetclinic.service;
 
 import com.ccube.vetnpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet Save(Pet pet);
-
-    Pet findById(Long id);
-
-    Set<Pet> findAll(Pet pet);
+public interface PetService extends CrudService<Pet, Long> {
 
 }
