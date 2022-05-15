9fad1a52d486a59e9f52001e51c5a0580bbd39d9
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-15 18:01:27 +0530

    implemented map based interfaces. closes #6.

---
+++ vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.ccube.vetnpetclinic.service.map;
+
+import com.ccube.vetnpetclinic.model.Owner;
+import com.ccube.vetnpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteByID(aLong);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ vetnpet-data-module/src/main/java/com/ccube/vetnpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.ccube.vetnpetclinic.service.map;
+
+import com.ccube.vetnpetclinic.model.Vet;
+import com.ccube.vetnpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteByID(aLong);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
