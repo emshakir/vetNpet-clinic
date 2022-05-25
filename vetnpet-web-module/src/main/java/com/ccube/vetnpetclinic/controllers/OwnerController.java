11df987342ec79604a5032d051d7f4c4d8c93e63
Mohammad Shakir <65658120+Shaquib47@users.noreply.github.com>
2022-05-25 22:03:47 +0530

    added list of owners in owners index page. closes#11.

--- vetnpet-web-module/src/main/java/com/ccube/vetnpetclinic/controllers/OwnerController.java
+++ vetnpet-web-module/src/main/java/com/ccube/vetnpetclinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package com.ccube.vetnpetclinic.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.ccube.vetnpetclinic.service.map.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"owners","owners/index","owners/index.html"})
-    public String vetIndex(Model model){
-        model.addAttribute("ownerWelcomeNote","List of Owner's Names");
+    private final OwnerServiceMap ownerServiceMap;
+
+    public OwnerController(OwnerServiceMap ownerServiceMap) {
+        this.ownerServiceMap = ownerServiceMap;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOfOwners(Model model) {
+        model.addAttribute("ownersList", ownerServiceMap.findAll());
         return "owners/index";
     }
 }
